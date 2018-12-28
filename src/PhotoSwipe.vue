48026b2b401d8aed0027c604200f6ed148b848d3
David Rosendo <david.rosendo@webds.pt>
2018-12-28 17:00:12 +0000

    Added ability for thumbnails with Flexslider (external source)

--- src/PhotoSwipe.vue
+++ src/PhotoSwipe.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Root element of PhotoSwipe. Must have class pswp. -->
-    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="pswp svipropswp" tabindex="-1" role="dialog" aria-hidden="true">
 
         <!-- Background of PhotoSwipe. 
             It's a separate element as animating opacity is faster than rgba(). -->
@@ -39,15 +39,15 @@
                     <!-- element will get class pswp__preloader--active when preloader is running -->
                     <div class="pswp__preloader">
                         <div class="pswp__preloader__icn">
-                        <div class="pswp__preloader__cut">
-                            <div class="pswp__preloader__donut"></div>
-                        </div>
+                            <div class="pswp__preloader__cut">
+                                <div class="pswp__preloader__donut"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                    <div class="pswp__share-tooltip"></div> 
+                    <div class="pswp__share-tooltip"></div>
                 </div>
 
                 <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
@@ -56,14 +56,19 @@
                 <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
                 </button>
 
-                <div class="pswp__caption">
+                <div class="pswp__caption" :class="[thumbnails?'svicaption-top':'']">
                     <div class="pswp__caption__center"></div>
                 </div>
 
             </div>
 
         </div>
-
+        <div v-if="thumbnails" class="arrow-down togglehidesvi" v-on:click='toggle()' onclick="this.classList.toggle('active')"></div>
+        <div v-if="thumbnails" class="photoSwipe_innerthumbs flexslider">
+            <ul class="slides">
+                <li v-for="(item,k) in items" :key='k' class='svili'><img :src="item.msrc" /></li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -73,38 +78,83 @@
 
     import PhotoSwipe from 'photoswipe/dist/photoswipe'
     import PhotoSwipeDefaultUI from 'photoswipe/dist/photoswipe-ui-default'
+    import flexslider from 'flexslider';
 
     export default {
+        data: function () {
+            return {
+                thumbnails: false,
+                items: []
+            }
+        },
+        mounted: function () {},
         methods: {
-            open (index, items, options = {
-                // captionEl: false,
-                fullscreenEl: true,
-                history: false,
-                shareEl: false,
-                tapToClose: true
-            }) {
-                const opts = Object.assign({
-                    index: index,
-                    getThumbBoundsFn (index) {
-                        const thumbnail = document.querySelectorAll('.preview-img-item')[index]
-                        const pageYScroll = window.pageYOffset
-                                || document.documentElement.scrollTop
-                                || document.body.scrollTop
-                                || 0
-                        const rect = thumbnail.getBoundingClientRect()
-                        return {
-                            x: rect.left,
-                            y: rect.top + pageYScroll,
-                            w: rect.width
-                        }
+            open(index, items, thumbnails, options) {
+                let _this = this;
+                this.thumbnails = thumbnails;
+                this.items = items;
+                this.photoswipe = new PhotoSwipe(this.$el, PhotoSwipeDefaultUI, items, options);
+                this.photoswipe.init();
+
+                var handle_visible = setInterval(function () {
+                    if ($('.pswp').is(":visible")) {
+                        $('.photoSwipe_innerthumbs,.togglehidesvi').hide();
+
+                        $.flexslider($('.photoSwipe_innerthumbs.flexslider'), {
+                            animation: "slide",
+                            controlNav: false,
+                            animationLoop: false,
+                            barsSize: {
+                                top: 44,
+                                bottom: 44
+                            },
+                            slideshow: true,
+                            itemWidth: 100,
+                            itemMargin: 5,
+                            prevText: "", //String: Set the text for the "previous" directionNav item
+                            nextText: "",
+                            start: function (slider) {
+                                if (slider.pagingCount === 1)
+                                    slider.addClass('flex-centered');
+
+                                _this.initClick();
+                            }
+                        });
+
+                        clearInterval(handle_visible);
                     }
-                }, options)
+                }, 5);
 
-                this.photoswipe = new PhotoSwipe(this.$el, PhotoSwipeDefaultUI, items, opts)
-                this.photoswipe.init()
+
             },
+            initClick() {
+                var _this = this;
+                var slider = $('.photoSwipe_innerthumbs');
+                $("div.photoSwipe_innerthumbs img").eq(_this.photoswipe.getCurrentIndex()).addClass('svifaded');
 
-            close () {
+                jQuery(_this.$el).on('click', 'div.photoSwipe_innerthumbs img', function (e) {
+                    $('div.photoSwipe_innerthumbs img').removeClass("svifaded");
+                    _this.photoswipe.goTo($("div.photoSwipe_innerthumbs img").index($(this)));
+                });
+
+                this.photoswipe.listen('afterChange', function () {
+                    $('div.photoSwipe_innerthumbs img').removeClass("svifaded");
+                    $("div.photoSwipe_innerthumbs img").eq(_this.photoswipe.getCurrentIndex()).addClass(
+                        'svifaded');
+                });
+
+                this.photoswipe.listen('close', function () {
+                    $('div.photoSwipe_innerthumbs img').removeClass("svifaded");
+                });
+
+                slider.slideDown();
+                $('.togglehidesvi').slideDown();
+            },
+            toggle() {
+                var slider = $('.photoSwipe_innerthumbs');
+                slider.toggle();
+            },
+            close() {
                 this.photoswipe.close()
             }
         }
